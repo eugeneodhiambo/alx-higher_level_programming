8e7d2849b4ff1e340ca8d260b4d8184dd52f7bac
eugeneodhiambo <eugeneodhiambo07@gmail.com>
2022-09-28 12:25:35 -0700

    Added task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,4 +4,4 @@ def replace_in_list(my_list, idx, element):
         return my_list
     else:
         my_list[idx] = element
-        return my_listurn my_listturn my_list[idx]
+        return my_list
